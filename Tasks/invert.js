bfc7ab9edddda5a625f789524bdcab8069cdcdec
Myroslava Saveniuk <saveniukf@gmail.com>
2024-01-14 17:15:27 +0200

    declarated function

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -2,7 +2,7 @@
 
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
+const invert = (A, i, j, k) => {
   T = Object.keys(A, 4);
   T.forEach((_, i) => {
     T[i] = A.pop();
