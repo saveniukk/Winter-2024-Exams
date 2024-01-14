c22a47c19d83fbfb0e6a7623fde6325ba29d511e
Myroslava Saveniuk <saveniukf@gmail.com>
2024-01-14 20:05:17 +0200

    added if-statement to check alphabet length

--- Tasks/password.js
+++ Tasks/password.js
@@ -3,6 +3,10 @@
 // Generate random password
 
 const generatePassword = (alphabet, length) => {
+  if (length <= 0) {
+    return '';
+  }
+  
   const indexMax = alphabet.length;
   let password = '';
   for (let i = 0; i < length; i++) {
