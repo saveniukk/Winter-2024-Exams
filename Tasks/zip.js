bb1a16408d3eb28f2b2be6b2f7f2500f72300d14
Myroslava Saveniuk <saveniukf@gmail.com>
2024-01-14 10:43:04 +0200

    added array to store result

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -3,18 +3,19 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
 const zip = (firstArr = [], secondArr = []) => {
+  let result = [];
   let i = 0;
   j = 0;
   for (x of secondArr) {
-    const cell = [firstArr[i++], x];
+    CELL = [firstArr[i++], x];
     if (i < j) {
       delete firstArr[i++];
     } else {
-      (() => (secondArr[j++] = cell))();
+      (() => (secondArr[j++] = CELL))();
     }
-    if (cell[0] == undefined) secondArr.length -= 1
+    if (CELL[0] == undefined) secondArr.length -= 1
   }
-  return secondArr;
+  return b;
 };
 
 module.exports = zip;
