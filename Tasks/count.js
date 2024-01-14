4edd5dfcf20d6c4205763d6aee2d75bab2c585da
Myroslava Saveniuk <saveniukf@gmail.com>
2024-01-14 17:09:58 +0200

    rewrited forEach to for...of loop

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,10 +5,14 @@
 const sumNum = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+
+  for (const key of keys) {
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
+    if (typeof value === 'number') {
+      sum += value;
+    }
+  }
+  
   return sum;
 };
 
