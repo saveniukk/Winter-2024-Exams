e9b965dce9a538ecb96ea7339de2eaa16d6e04a3
Myroslava Saveniuk <saveniukf@gmail.com>
2024-01-14 22:39:03 +0200

    agjustet spaces

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,27 +1,26 @@
-'use strict';
+"use strict";
 
 // Replace substring with newstr
 
 const replace = (originalStr, targetSubstring, newSubstring) => {
-  if (targetSubstring === '' || !originalStr.includes(targetSubstring)) {
+  if (targetSubstring === "" || !originalStr.includes(targetSubstring)) {
     return originalStr;
   }
   let remainingStr = originalStr;
-  let result = '';
+  let result = "";
 
-    while (remainingStr.includes(targetSubstring)) {
-      const index = remainingStr.indexOf(targetSubstring);
-      const startLength = index;
-      const targetLength = targetSubstring.length;
+  while (remainingStr.includes(targetSubstring)) {
+    const index = remainingStr.indexOf(targetSubstring);
+    const startLength = index;
+    const targetLength = targetSubstring.length;
 
-      const start = remainingStr.substring(0, startLength);
-      remainingStr = remainingStr.substring(index + targetLength);
-        
-      result += start + newSubstring;
-      
-    } 
-    return result + remainingStr;
+    const start = remainingStr.substring(0, startLength);
+    remainingStr = remainingStr.substring(index + targetLength);
 
+    result += start + newSubstring;
+  }
+  return result + remainingStr;
 };
 
 module.exports = replace;
+ 
