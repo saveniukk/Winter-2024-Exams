29c27f618886c9e32a29e334774444f570e3af3a
Myroslava Saveniuk <saveniukf@gmail.com>
2024-01-14 19:08:04 +0200

    changed function declaration

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,7 +2,7 @@
 
 // Find an intersection of two dictionaries
 
-intersection = function intersection(obj1, obj2) {
+const intersection = (obj1, obj2) => {
    obj1Keys = Object.keys(obj1);
   for (attribute_name of   obj1Keys) {
     if (obj1[attribute_name] === obj2[attribute_name]) {
