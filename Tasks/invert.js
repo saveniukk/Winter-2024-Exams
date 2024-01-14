21d61f1673eff3a630c7096bb811657998066130
Myroslava Saveniuk <saveniukf@gmail.com>
2024-01-14 17:21:25 +0200

    removed unnecessary parts

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -2,14 +2,11 @@
 
 // Reverse an array, you can't use .reverse()
 
-const invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
+const invert = (A) => {
+  T = Object.keys(A);
   T.forEach((_, i) => {
     T[i] = A.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
+  });
   return T;
 };
 
